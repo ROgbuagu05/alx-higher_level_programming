ebb445f06300e2f8a591e97c31c98c0c1b94f85c
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-06 14:07:36 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) not in ['e', 'q']:
-        print(f"{chr(letter)}", end="")
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) not in ['e', 'q']:
+        print("{:c}".format(i), end="")
