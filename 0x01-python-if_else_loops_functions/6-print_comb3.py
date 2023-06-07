a54295daa59ff9feccdb668b209ceee73263463d
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-07 13:32:22 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-num = 0
-while num <= 89:
-    if num % 10 == 0 and num != 0:
-        num += 1 + num // 10
-    print("{:02d}".format(num), end=', ' if num != 89 else '\n')
-    num += 1
+number = 0
+while number <= 89:
+    if number % 10 == 0 and number != 0:
+        number += 1 + number // 10
+    print("{:02d}".format(number), end=', ' if number != 89 else '\n')
+    number += 1
