0e4fabeccc82bea90894832422c40bfa0b9a2c9e
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-10 04:46:07 -0700

    7-add_tuple.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """Prints a matrix of integers."""
+    if matrix is None:
+        matrix = []
+
     for row in matrix:
         for integer in row:
             print(str.format("{0}", integer), end=" ")
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-  """Adds 2 tuple."""
-  a = tuple_a + (0, 0)
-  b = tuple_b + (0, 0)
-  result = (a[0] + b[0], a[1] + b[1])
-  return (result)
+    """Adds 2 tuple."""
+    a = (tuple_a + (0, 0))[:2]
+    b = (tuple_b + (0, 0))[:2]
+    return (a[0] + b[0], a[1] + b[1])
