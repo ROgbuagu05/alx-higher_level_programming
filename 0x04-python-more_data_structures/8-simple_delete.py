06702c68dc802417912a6b8f1e2c57fc063440f6
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-14 13:31:05 -0700

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Deletes a key in a dictionary."""
 def simple_delete(a_dictionary, key=""):
+    """Deletes a key in a dictionary."""
     if key in a_dictionary is not None:
         a_dictionary.pop(key)
     return (a_dictionary)
