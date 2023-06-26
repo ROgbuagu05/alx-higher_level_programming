301a1787737dfb215a8ee417b47df23df8ea41c2
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-26 03:36:17 -0700

    5-raise_exception.py

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def raise_exception():
+    """
+    Raises a type exception.
+
+    Args:
+        No arguments.
+
+    Returns:
+        Nothing
+    """
+    raise TypeError("This is a type exception")
