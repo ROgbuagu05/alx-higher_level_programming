d4a695a83e7cc468fde4b1ba0bc2f7240a8653aa
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-26 05:25:57 -0700

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -18,3 +18,5 @@ def safe_print_integer_err(value):
     except (ValueError, TypeError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return (False)
+    else:
+        return (True)
