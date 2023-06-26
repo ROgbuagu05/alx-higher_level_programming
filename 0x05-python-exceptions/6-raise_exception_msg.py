d7879e7cd8196240573d70d9ed03131cac9f8beb
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-26 03:43:16 -0700

    6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    Raises a name exception with a message.
+
+    Args:
+        message: The message to be included in the exception.
+
+
+    Returns:
+        Nothing.
+    """
+    raise NameError(message)
