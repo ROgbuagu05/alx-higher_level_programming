9dee4ea7f3e98ee511b530ffa0d7a8f60359755d
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-27 09:01:09 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
+    """
+    This class defines a square by its size.
+    """
     def __init__(self, size):
+        """
+        The constructor for Square class.
+
+        Parameters:
+            size (int): The size of the square.
+        """
         self.__size = size
