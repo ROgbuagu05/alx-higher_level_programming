a994371bffaee28e5a21ca037c01e15848ade88d
ROgbuagu05 <chizzy3003@gmail.com>
2023-06-27 10:41:21 -0700

    102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This class defines a square by its size.
+    """
     def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+        size (int): The size of the square.
+        """
         self.__size = size
 
     @property
