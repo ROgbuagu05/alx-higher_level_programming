00d7d54a222cb782d1bcc898d677bb9333d2a492
ROgbuagu05 <chizzy3003@gmail.com>
2023-08-16 03:42:53 -0700

    7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- A script that creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use the active database
+USE hbtn_0d_usa;
+-- Create the table cities if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE(id),
+    FOREIGN KEY(state_id)
+        REFERENCES states(id)
+);
