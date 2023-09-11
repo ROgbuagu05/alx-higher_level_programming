87ff8f3a9693811b9e12893bef5be11c1ae524f2
ROgbuagu05 <chizzy3003@gmail.com>
2023-09-11 04:30:42 -0700

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-if (!process.argv[2] || !Number(process.argv[2])) {
-	console.log('Not a number');
-} else {
-	console.log('My number: ' + Number(process.argv[2]));
-}
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
