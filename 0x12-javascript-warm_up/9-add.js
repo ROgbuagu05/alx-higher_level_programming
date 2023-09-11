ae7e0b6377256134f0a2734e3e5b55c8f0871ac9
ROgbuagu05 <chizzy3003@gmail.com>
2023-09-11 04:18:47 -0700

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const a = parseInt(args[0]);
-const b = parseInt(args[1]);
-
-function add(a, b) {
+function add (a, b) {
 	return a + b;
 }
 
-console.log(add(a, b));
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
