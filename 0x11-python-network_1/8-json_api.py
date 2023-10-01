0571f41f336810554cfd10ef4c9f789d6bfe6d81
ROgbuagu05 <chizzy3003@gmail.com>
2023-10-01 07:44:04 -0700

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    try:
+        data = response.json()
+        if data:
+            for user in data:
+                print(f"[{user['id']}] {user['name']}")
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
