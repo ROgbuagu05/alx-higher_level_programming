ba8626c99cb436952889ccf33bbafbe77790ecbf
ROgbuagu05 <chizzy3003@gmail.com>
2023-10-24 04:38:19 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,13 +3,14 @@
  * A script that writes a string to a file.
  */
 
+const process = require('process');
 const fs = require('fs');
 
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-	if (err) {
-		console.log(err);
-	}
+fs.writeFile(filePath, content, 'utf-8', err => {
+    if (err) {
+        console.log(err);
+    }
 });
