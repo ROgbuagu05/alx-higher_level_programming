c6cbc55024d22667cb1f9c5d1f3c8caccd31870b
ROgbuagu05 <chizzy3003@gmail.com>
2023-10-24 05:27:48 -0700

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,21 +1,9 @@
 #!/usr/bin/node
-/**
- * A script that gets the contents
- * of a webpage and stores it in a file.
- */
 const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
 
-request.get(url, (error, response, body) => {
-	if (error) {
-		fs.writeFile(filePath, body, 'utf-8', fileError => {
-			if (fileError !== null) {
-				console.log(fileError);
-			}
-		});
-	} else {
-		console.error(error);
-	}
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
 });
