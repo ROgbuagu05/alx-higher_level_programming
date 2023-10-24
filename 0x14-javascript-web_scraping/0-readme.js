124b82d2a24b6227133d479474d68b710d8e6e44
ROgbuagu05 <chizzy3003@gmail.com>
2023-10-24 04:42:03 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /**
- * A script that reads and prints the content of a file.
+ * Script that reads and prints the content of a file.
  */
 const process = require('process');
 const fs = require('fs');
